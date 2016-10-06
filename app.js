959543f0c8759f7b07e2bbb99c27facae253da31
athattayathu <anilthattayathu@gmail.com>
2016-10-05 17:05:30 -0700

    fixed static file access issues

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'js')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
