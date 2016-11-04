3109b4cd3b651178263340481b57bff4ce4f9be2
athattayathu <anilthattayathu@gmail.com>
2016-11-04 00:03:37 -0700

    utility time changes

--- js/utility.js
+++ js/utility.js
@@ -11,4 +11,25 @@ class Utility{
 		}
 		return url;
 	}
+
+	/**
+	 * Create a string of the format hh:mm:ss or mm:ss from time in seconds passed in
+	 **/
+	static timeToStr(time){
+
+		const MINUTE_TO_SECOND = 60;
+		const HOUR_TO_SECOND = 3600;
+
+		var hour = Math.trunc(time /HOUR_TO_SECOND);
+
+		var minute = Math.trunc(time / MINUTE_TO_SECOND);
+		var seconds = time % MINUTE_TO_SECOND;
+		seconds = (seconds < 10)? '0' + seconds : seconds;
+
+		var res =  minute + ':' + seconds ;
+		res = (hour) ? (hour+":"+res) : res;
+	        console.log(time);
+		console.log(res);
+		return res;
+	}
 }
